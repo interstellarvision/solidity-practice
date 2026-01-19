a4e63dc7d73775cd70ccc535ea7c7c53e79813e1
Владислав Крапко <vladislavkrapko@nostylist.local>
2026-01-19 19:25:54 +0300

    Add MyToken skeleton (WIP)

---
+++ src/MyToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MyToken {
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+        emit Transfer(address(0), msg.sender, _initialSupply);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        // TODO: твой код здесь
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        // TODO: твой код здесь
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        // TODO: твой код здесь
+    }
+}
